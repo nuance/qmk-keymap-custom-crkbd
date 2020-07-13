924b0a292b867bb2e505581830e157296d44eb2b
Matthew Jones <matt@mhjones.org>
2020-07-13 14:45:55 -0400

    Remove extra layers

--- crkbd-custom/keymap.c
+++ crkbd-custom/keymap.c
@@ -2,11 +2,6 @@
 
 extern keymap_config_t keymap_config;
 
-#ifdef RGBLIGHT_ENABLE
-//Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
-
 #ifdef OLED_DRIVER_ENABLE
 static uint32_t oled_timer = 0;
 #endif
@@ -20,9 +15,7 @@ extern uint8_t is_master;
 enum layers {
   _QWERTY,
   _LOWER,
-  _RAISE,
-  _ADJUST,
-  _NAV
+  _RAISE
 };
 
 // Custom keycodes for layer keys
@@ -34,20 +27,33 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
-  NAV,
   RGBRST,
   KC_RACL // right alt / colon
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/*
+ * Default Layer
+ *
+ * .-------------------------------------------.                  .-------------------------------------------.
+ * |GUI/ESC |   Q  |  W   |  E   |  R   |  T   |                  |  Y   |  U   |  I   |  O   |  P   |  BPSC  |
+ * |--------+------+------+------+------+------|                  |------+------+------+------+------+--------|
+ * |CTR/TAB |   A  |  S   |  D   |  E   |  F   |                  |  H   |  J   |  K   |  L   |  L   |   '    |
+ * |--------+------+------+------+------+------+                  |------+------+------+------+------+--------|
+ * |SHIFT/( |   Z  |  X   |  C   |  V   |  B   |                  |  N   |  M   |  ,   |  .   |  /   |SHIFT/) |
+ * '----------------------+------+------+------+                  |------+------+------+----------------------'
+ *                        |      |      |      |                  |      |      |      |
+ *                        | CMD  | LOW  |SHF/  |                  |MET/E |  UP  | LEAD |
+ *                        '--------------------'                  '--------------------'
+ */
+
   [_QWERTY] = LAYOUT(
   //,-----------------------------------------.                ,---------------------------------------------.
     KC_LGESC,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
 CTL_T(KC_TAB),  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-    KC_LSPO,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,   NAV,
+    KC_LSPO,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_RSPO,
   //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------|
                 KC_LGUI, LOWER, MT(MOD_LSFT, KC_SPC),   MT(MOD_LALT, KC_ENT), RAISE, KC_LEAD
                               //`--------------------'  `--------------------'
@@ -55,7 +61,7 @@ CTL_T(KC_TAB),  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,
 
 /*
  * Lower Layer: Numpad, Media
- * 
+ *
  * .-------------------------------------------.                  .-------------------------------------------.
  * |        |  F7  | F8   | F9   | F10  |      |                  | / ?  | 7 &  | 8 *  | 9 (  | - _  |        |
  * |--------+------+------+------+------+------|                  |------+------+------+------+------+--------|
@@ -92,43 +98,12 @@ CTL_T(KC_TAB),  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,
    _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,         KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_PERC, KC_QUOT,
    _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,        KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
                           _______, _______, _______,        _______, _______, _______
-  ),
-
-  [_ADJUST] = LAYOUT(
-  //,-----------------------------------------.                ,-----------------------------------------.
-      RESET,RGBRST, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO,KC__MUTE, KC_NO, KC_NO, KC_NO, KC_NO,
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,RGB_SPI,KC_NO,               KC_PAUSE,KC__VOLUP, KC_NO, KC_NO, KC_NO, KC_NO,
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,RGB_SPD,KC_NO,               KC_SCROLLLOCK,KC__VOLDOWN, KC_NO, KC_NO, KC_NO, RGB_RMOD,
-  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT
-                              //`--------------------'  `--------------------'
-  ),
-
-  [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, KC_SLCK,
-CTL_T(KC_TAB), _______, _______, _______, _______, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_CAPS,
-      _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-                                          _______, _______, _______,    _______, _______, _______
   )
 };
 
 int RGB_current_mode;
 
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
-
 void matrix_init_user(void) {
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
 }
 
 #ifdef OLED_DRIVER_ENABLE
@@ -292,24 +267,11 @@ void render_layer_state(void) {
         0x20, 0x9a, 0x9b, 0x9c, 0x20,
         0x20, 0xba, 0xbb, 0xbc, 0x20,
         0x20, 0xda, 0xdb, 0xdc, 0x20, 0};
-    static const char PROGMEM adjust_layer[] = {
-        0x20, 0x9d, 0x9e, 0x9f, 0x20,
-        0x20, 0xbd, 0xbe, 0xbf, 0x20,
-        0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
 
-    static const char PROGMEM nav_layer[] = {
-        0x20, 0x9d, 0x9e, 0x9f, 0x20,
-        0x20, 0xba, 0xbb, 0xbc, 0x20,
-        0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
-
-    if(layer_state_is(_ADJUST)) {
-        oled_write_P(adjust_layer, false);
-    } else if(layer_state_is(_LOWER)) {
+    if(layer_state_is(_LOWER)) {
         oled_write_P(lower_layer, false);
     } else if(layer_state_is(_RAISE)) {
         oled_write_P(raise_layer, false);
-    } else if(layer_state_is(_NAV)) {
-        oled_write_P(nav_layer, false);
     } else {
       oled_write_P(default_layer, false);
     }
@@ -363,35 +325,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       }
       return false;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       }
       return false;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-    case NAV:
-        if (record->event.pressed) {
-          layer_on(_NAV);
-        } else {
-          layer_off(_NAV);
-        }
-        return false;
     case KC_RACL:
         if (record->event.pressed) {
           my_colon_timer = timer_read();
@@ -421,15 +365,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-
-#ifdef RGB_MATRIX_ENABLE
-
-void suspend_power_down_keymap(void) {
-    rgb_matrix_set_suspend_state(true);
-}
-
-void suspend_wakeup_init_keymap(void) {
-    rgb_matrix_set_suspend_state(false);
-}
-
-#endif
